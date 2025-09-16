465a92d95e0f75ebf7b221c524fffa3428e574c6
Adrien <adrienleyval4@gmail.com>
2025-09-16 16:01:33 +0200

    chore(front): add axios and test button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-    return <button className="text-3xl">Hello</button>;
+    return (
+        <button className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110 ">
+            Tester la connexion
+        </button>
+    );
 }
 
 export default App;
