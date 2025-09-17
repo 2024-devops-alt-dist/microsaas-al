a833bb3cf561e71f9840ee14e75355fab2bea236
Adrien <adrienleyval4@gmail.com>
2025-09-17 12:31:11 +0200

    feat: initialise basic server

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -8,10 +8,7 @@ export default defineConfig([
     globalIgnores(['dist']),
     {
         files: ['**/*.{ts,tsx}'],
-        extends: [
-            eslint.configs.recommended,
-            tseslint.configs.recommended,
-        ],
+        extends: [eslint.configs.recommended, tseslint.configs.recommended],
         languageOptions: {
             ecmaVersion: 2020,
         },
