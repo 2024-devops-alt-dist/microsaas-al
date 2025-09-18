2e4092ccb0e1e7cb3bc61d639730d202eb4e3d0d
Adrien <adrienleyval4@gmail.com>
2025-09-18 16:25:53 +0200

    feat(healt): add health check api call and page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
+import HealthPage from './features/health/healthPage';
 
 function App() {
     return (
-        <button className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110 ">
-            Tester la connexion
-        </button>
+        <HealthPage />
     );
 }
 
