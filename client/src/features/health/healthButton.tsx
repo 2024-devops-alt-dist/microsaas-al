cfaa9aa654d47d10dea6bce70480b7ebbdbacad6
Adrien <adrienleyval4@gmail.com>
2025-10-13 12:15:13 +0200

    chore(lefthook): add lefhook config and commitlint

--- client/src/features/health/healthButton.tsx
+++ client/src/features/health/healthButton.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import { getHealthStatus } from "./healthApi";
+import { useState } from 'react';
+import { getHealthStatus } from './healthApi';
 
 export function HealthButton() {
-    const [healthStatus, setHealthStatus] = useState<{ status: string, message: string }>({ status: '', message: '' });
+    const [healthStatus, setHealthStatus] = useState<{ status: string; message: string }>({
+        status: '',
+        message: '',
+    });
 
     async function testDbConnection() {
         try {
@@ -16,7 +19,10 @@ export function HealthButton() {
 
     return (
         <>
-            <button onClick={testDbConnection} className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110 ">
+            <button
+                onClick={testDbConnection}
+                className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110 "
+            >
                 Tester la connexion
             </button>
             <p>{healthStatus.message}</p>
--- client/src/features/health/healthPage.tsx
+++ client/src/features/health/healthPage.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { HealthButton } from './healthButton';
 
 const HealthPage: React.FC = () => {
-  return (
-    <div>
-      <h2>Health Check</h2>
-      <HealthButton />
-    </div>
-  );
+    return (
+        <div>
+            <h2>Health Check</h2>
+            <HealthButton />
+        </div>
+    );
 };
 
 export default HealthPage;
